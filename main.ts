bce73255dd3d8ec1bd90c2877b48937e9ab0b738
jlab001 <35016493+jlab001@users.noreply.github.com>
2018-03-08 13:48:51 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,94 +1,120 @@
-/*
-Copyright 2018 Jack Ho
+/* The MIT License (MIT)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+Copyright (c) 2017 Riven
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies
-or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
-AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
-let buf = pins.createBuffer(8)
 
-enum MyEnum {
-    //% block="001001.mp3"
-    One = 1,
-    //% block="002002.mp3"
-    Two = 2,
-    //% block="003003.mp3"
-    Three = 3,
-    //% block="004004.mp3"
-    Four = 4,
-     //% block="005005.mp3"
-    Five = 5,
-     //% block="006006.mp3"
-    Six = 6,
-    //% block="007007.mp3"
-    Seven = 7,
-    //% block="008008.mp3"
-    Eight = 8
+//% color="#ff7300" weight=10 icon="\uf110"
+namespace ServoDriver {
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const LED0_ON_L = 0x06
+    const PRESCALE = 0xFE
 
-}
+    let initialized = false
 
-/**
- * Custom blocks
- */
-//% weight=5 color=#2699BF icon="\uf110"
-namespace MP3 {
-    /**
-     * TODO: describe your function here
-     * @param RX connects to RX pin of the module
-     * @param TX connects to TX pin of the module
-     */
-       //% blockId=mp3_init block="Initialise MP3 module |RX: %RX| TX: %TX"
-    //% weight=89
-    export function InitialiseMP3(RX: SerialPin, TX: SerialPin): void {
-        // Add code here
-        buf.setNumber(NumberFormat.UInt8LE, 0, 0x7e)
-        buf.setNumber(NumberFormat.UInt8LE, 1, 0xff)
-        buf.setNumber(NumberFormat.UInt8LE, 2, 0x06)
-        buf.setNumber(NumberFormat.UInt8LE, 3, 0x09)
-        buf.setNumber(NumberFormat.UInt8LE, 4, 0x01)
-        buf.setNumber(NumberFormat.UInt8LE, 5, 0x00)
-        buf.setNumber(NumberFormat.UInt8LE, 6, 0x02)
-        buf.setNumber(NumberFormat.UInt8LE, 7, 0xef)
-        serial.redirect(
-            RX,
-            TX,
-            BaudRate.BaudRate9600
-        )
-        serial.writeBuffer(buf)
-
+    export enum Servos {
+        C0 = 0x00,
+        C1 = 0x01,
+        C2 = 0x02,
+        C3 = 0x03,
+        C4 = 0x04,
+        C5 = 0x05,
+        C6 = 0x06,
+        C7 = 0x07,
+        C8 = 0x08,
+        C9 = 0x09,
+        C10 = 0x0A,
+        C11 = 0x0B,
+        C12 = 0x0C,
+        C13 = 0x0D,
+        C14 = 0x0E,
+        C15 = 0x0F,
     }
 
-    /**
-     * TODO: Use the block to play a mp3 file in the folder 01/ in microSD card.
-     * @param value describe value here, eg: 5
-     */
-        //% blockId=mp3_play block="Play MP3 file %RX| in folder /01"
-    //% weight=89
 
-    export function PlayMP3(value: MyEnum) {
-        buf.setNumber(NumberFormat.UInt8LE, 0, 0x7e)
-        buf.setNumber(NumberFormat.UInt8LE, 1, 0xff)
-        buf.setNumber(NumberFormat.UInt8LE, 2, 0x06)
-        buf.setNumber(NumberFormat.UInt8LE, 3, 0x0f)
-        buf.setNumber(NumberFormat.UInt8LE, 4, 0x00)
-        buf.setNumber(NumberFormat.UInt8LE, 5, 0x01)
-        buf.setNumber(NumberFormat.UInt8LE, 6, value)
-        buf.setNumber(NumberFormat.UInt8LE, 7, 0xef)
-        serial.writeBuffer(buf)
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+    //% blockId=Servo_Driver block="servo write pin |%index| to %degree"
+    //% weight=100
+    //% blockGap=50
+    //% degree.min=0 degree.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=8
+    export function Servo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index, 0, value)
     }
 }
-
-serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {
-    basic.showString(serial.readLine())
-})
